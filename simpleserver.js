c97b87be395c7d48c96f8bdccbdc76a8f4b3ce3b
npxl32 <154693441+npxl32@users.noreply.github.com>
2023-12-23 10:11:10 -0800

    Simple Server

---
+++ simpleserver.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const url = require("url");
+// i have the free will to program in other langauges even if I say im a dotnet dev
+// also i have the free will to write terrible code :)
+const app = http.createServer((req, res) => {
+    var boilerplate_begin = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\" /><style>html {height: auto; overflow-x: hidden;} body {background-color: black; color: white; height: 100%;} footer {width: 100%; bottom: 0px; position: absolute;}</style></head><body>";
+    if(url.parse(req.url).pathname == "/npxl32") {
+        res.write(boilerplate_begin);
+        res.write("<h1>Secret Page</h1>");
+        res.write("<p>This page exists!</p>");
+        res.write("<footer>");
+        res.write("<hr />");
+        res.write("<a href=\"privacy\">Privacy</a>");
+        res.write("</footer>");
+        res.write("</body></html>");
+        res.end();
+    } else if(url.parse(req.url).pathname == "/privacy") {
+        res.write(boilerplate_begin);
+        res.write("<h1>Privacy Policy</h1>");
+        res.write("<h3>Do you collect any info?</h2>");
+        res.write("<p>No.</p>");
+        res.write("<h3>Do you use cookies?</h2>");
+        res.write("<p>No.</p>");
+        res.write("<footer>");
+        res.write("<hr />");
+        res.write("<a href=\"privacy\">Privacy</a>");
+        res.write("</footer>");
+        res.write("</body></html>");
+        res.end();
+    } else {
+        res.write(boilerplate_begin);
+        res.write("<h1>npxl32</h1>");
+        res.write("<p>Work in progress!</p>");
+        res.write("<small>And yes, the domain won't look like this.</small>");
+        res.write("<footer>");
+        res.write("<hr />");
+        res.write("<a href=\"privacy\">Privacy</a>");
+        res.write("</footer>");
+        res.write("</body></html>");
+        res.end();
+    }
+});
+app.listen(8080, () => console.log("I exist!!1"));
